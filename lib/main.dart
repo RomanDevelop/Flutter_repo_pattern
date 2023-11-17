39d6504c708301d7320f66b31c568f93ebf4a121
roman.k <roman.k@3sda-evayadatasystems.com>
2023-11-17 19:22:43 +0100

    Added connection

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_repopattern/data/network/client/api_client.dart';
 import 'package:flutter_repopattern/data/network/network_mapper.dart';
+import 'package:flutter_repopattern/data/repository/movies_repository.dart';
 import 'package:flutter_repopattern/domain/model/config.dart';
+import 'package:flutter_repopattern/presentation/app/app.dart';
 import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 class InitialData {
@@ -15,8 +18,12 @@ class InitialData {
   InitialData({required this.providers});
 }
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final data = await _createData();
+
+  runApp(App(data: data));
 }
 
 Future<InitialData> _createData() async {
@@ -36,6 +43,18 @@ Future<InitialData> _createData() async {
     apiHost: config.apiHost,
   );
   final networkMapper = NetworkMapper(log: log);
+  final movieRepo = MoviesRepository(
+    apiClient: apiClient,
+    networkMapper: networkMapper,
+  );
+
+  // Create and return list of providers
+  return InitialData(
+    providers: [
+      Provider<Logger>.value(value: log),
+      Provider<MoviesRepository>.value(value: movieRepo),
+    ],
+  );
 }
 
 Future<Config> _loadConfig(Logger log) async {
--- lib/presentation/app/app.dart
+++ lib/presentation/app/app.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_repopattern/main.dart';
 import 'package:flutter_repopattern/presentation/list/movies_list_screen.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final InitialData data;
+
+  const App({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Movie App',
-      home: MoviesListScreen(),
+    return MultiProvider(
+      providers: data.providers,
+      child: const MaterialApp(
+        title: 'Movie App',
+        home: MoviesListScreen(),
+      ),
     );
   }
 }
